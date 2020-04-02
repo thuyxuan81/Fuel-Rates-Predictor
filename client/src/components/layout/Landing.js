f3c8df716ce97abbfc382640b41a01e1a8ca5b20
Thuy Nguyen <thuyxuan.nguyen81@gmail.com>
2020-04-02 14:19:24 -0500

    Adding frontend landing and navbar page

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Landing = () => {
+    return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Fuel Rate Predictor</h1>
+          <p className="lead">
+          </p>
+          <div className="buttons">
+            <a href="register.html" className="btn btn-primary">Sign Up</a>
+            <a href="login.html" className="btn btn-light">Login</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    )
+}
+
+export default Landing
